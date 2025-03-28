26d6ba4ed726565701eb04633fd141589b1f2914
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-03-28 12:10:57 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Trixie is my first attempt at making a virtual assistant.
+
+need copy Trixie's files into GitHub.
+can tKinter work within GitHub?
---
+++ script.js
@@ -0,0 +1,9 @@
+const person = document.getElementById(person);
+const greeting = document.getElementById(greeting);
+const question = document.getElementById(question);
+
+const names = ["Mark", "Chris", "Katy", "Yashi", "Tasha", "Ning"];
+
+// get time of day to determine the greeting
+// show chunk1 for 2-3 sec before displaying chunk2 instead.
+// idr what i was planning for chunk3...
